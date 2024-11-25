4b4f57da9fc23ac32fe02d97fe9cb88fd8d2d713
Daniel <danielchm6@gmail.com>
2024-11-25 18:58:35 -0500

    Configure index.php as central controller to handle all actions

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-$pageContent = 'Views/Home/index.php'; // Set the page-specific content
-include "Views/Shared/_Layout.php";
+    # require('model/database.php');
+
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+    if (!$action) {
+        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+    }
+
+    switch($action) {
+        case "tops": 
+            include 'views/catalog/tops.php';
+            break;
+        default:
+            $pageContent = 'Views/Home/index.php'; // Set the page-specific content
+            include "Views/Shared/_Layout.php";
+    }
+
 ?>
-   
---
+++ views/catalog/tops.php
@@ -0,0 +1,6 @@
+<main>
+        <section id="home">
+            <h1 style="text-align: center;">Welcome to our tops!</h1>
+            <p></p>
+        </section>
+</main>
