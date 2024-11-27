2c3976269cc57bc713cfc8849e06eacd7e97ca2d
Daniel <danielchm6@gmail.com>
2024-11-26 21:40:55 -0500

    Edit controllers to extend BaseController class to return views

--- controllers/accountController.php
+++ controllers/accountController.php
@@ -1,22 +1,20 @@
 <?php
+include_once "base/baseController.php";  // Adjust the path as needed
 
-class AccountController
+class AccountController extends BaseController
 {
     public function index($id = null)
     {
-        $pageContent = 'Views/account/index.php'; // Set the page-specific content
-        include "Views/Shared/_Layout.php";
+        $this->view('index');
     }
 
     public function register($id = null)
     {
-        $pageContent = 'Views/account/register.php'; // Set the page-specific content
-        include "Views/Shared/_Layout.php";
+        $this->view('register');
     }
 
     public function login($id = null)
     {
-        $pageContent = 'Views/account/login.php'; // Set the page-specific content
-        include "Views/Shared/_Layout.php";
+        $this->view('login');
     }
 }
