4c8cce74071974514ef37a9469986cd897e8e92d
Bennett Ferrari <baferrar@syr.edu>
2024-11-27 15:54:45 -0500

    Remove un-needed code + fix controller + add inventory model

--- controllers/adminController.php
+++ controllers/adminController.php
@@ -1,18 +1,24 @@
 <?php
 include_once "base/baseController.php";
-include_once "viewModels/admin/manageProductsViewModel.php";
+include_once "viewModels/admin/manageInventoryViewModel.php";
+include_once "models/inventory.php";
 include_once "models/db.php";
 
 class AdminController extends BaseController
 {
-    // View all products
-    public function manageProducts()
-    {
-        // Initialize the ViewModel
-        $manageProductsViewModel = new ManageProductsViewModel();
-        
-        // Get all products from the database
-        $manageProductsViewModel->getProducts();
 
+    public function ManageInventory()
+    {
+        $Inventory = new Inventory();
+        //connect
+        $dbContext = getDatabaseConnection();
+        
+        //sql query for the products
+        $query = "SELECT * FROM products";
+        $statement = $dbContext->query($query);
+
+        //fetch them all
+        $Inventory->products = $statement->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($Inventory->products);
     }
 }
--- models/product.php
+++ models/product.php
@@ -8,13 +8,4 @@ class Product
     public $stock_quantity;
     public $categoryID;
 
-    public function getAllProducts()
-    {
-        include "db.php";
-        $dbContext = getDatabaseConnection();
-        $statement = $dbContext->prepare("SELECT * FROM products");
-        $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
 }
