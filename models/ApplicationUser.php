69ad0cb124b8751b5e8f3a6efbf7de15a48cda2e
Daniel <danielchm6@gmail.com>
2024-11-27 19:34:28 -0500

    Add fields to ApplicationUser model

--- models/ApplicationUser.php
+++ models/ApplicationUser.php
@@ -1,11 +1,14 @@
 <?php
 class ApplicationUser
 {
+    public $userID = '';
     public $first_name = '';
     public $last_name = '';
     public $email = '';
     public $password = '';
-    public $confirm_password = '';
+    public $shipping_address = '';
+    public $billing_address = '';
+    public $phone_number = '';
     public $user_type = '';
     public $created_date = '';
 }
