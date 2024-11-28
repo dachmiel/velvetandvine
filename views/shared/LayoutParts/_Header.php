27b4adc25845864db9ca2b2f08c1c0e9641b6f26
Daniel <danielchm6@gmail.com>
2024-11-28 18:45:42 -0500

    Update Navbar and Header

--- views/shared/LayoutParts/_Header.php
+++ views/shared/LayoutParts/_Header.php
@@ -1,14 +1,110 @@
 <div class="header-custom">
-  <div class="container pb-3">
-    <div class="row">
-      <div class="col d-flex justify-content-center align-items-center">
+  <div class="container position-relative">
+    <div class="d-flex align-items-center justify-content-between">
 
-        <!-- Logo as clickable link -->
-        <a class="navbar-brand" href="/velvetandvine" style="display: block;">
-          <img src="/velvetandvine/images/logo.png" title="Velvet&Vine" alt="Velvet&VineLogo" style="max-width: 200px; height: auto; display: block;" />
-        </a>
-
+      <!-- Left: Search Bar -->
+      <div class="search-bar d-flex align-items-center">
+        <form class="search-form">
+          <input type="search" class="form-control" placeholder="Search">
+        </form>
       </div>
+
+      <!-- Center: Logo (Absolutely Centered) -->
+      <div class="logo-container position-absolute top-50 start-50 translate-middle">
+        <a class="navbar-brand" href="/velvetandvine">
+          <img src="/velvetandvine/images/logo.png"
+            alt="Velvet&Vine Logo"
+            style="max-width: 200px; height: auto;" />
+        </a>
+      </div>
+
+      <!-- Right: Dynamic Content -->
+      <div class="auth-buttons d-flex align-items-center">
+        <?php
+        if ($this->isAuthenticated() && $this->isAdmin()) {
+        ?>
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Admin
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/profile.php">Profile</a></li>
+                <li><a class="dropdown-item" href="/velvetandvine/admin/manageinventory">Manage Inventory</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="/velvetandvine/views/account/logout.php">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        <?php
+        } else if ($this->isAuthenticated() && !$this->isAdmin()) {
+        ?>
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Settings
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/profile.php">Profile</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="/velvetandvine/views/account/logout.php">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        <?php
+        } else {
+        ?>
+          <ul class="navbar-nav d-flex flex-row gap-2">
+            <li class="nav-item">
+              <a class="auth-link me-3" href="/velvetandvine/account/register">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="auth-link" href="/velvetandvine/account/login">Login</a>
+            </li>
+          </ul>
+        <?php
+        }
+        ?>
+      </div>
+
     </div>
   </div>
 </div>
+
+<style>
+  .header-custom {
+    background-color: #faf7f0;
+    padding: 40px 0;
+  }
+
+  .header-custom .logo-container {
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+  }
+
+  .header-custom .search-bar,
+  .header-custom .auth-buttons {
+    flex-shrink: 0;
+  }
+
+  .search-form .form-control {
+    border-radius: 20px;
+    width: 250px;
+    padding-left: 15px;
+  }
+
+  .auth-buttons .auth-link {
+    color: black;
+    text-decoration: none;
+    font-weight: 500;
+  }
+
+  .auth-buttons .auth-link:hover {
+    text-decoration: underline;
+  }
+</style>
--- views/shared/LayoutParts/_Navbar.php
+++ views/shared/LayoutParts/_Navbar.php
@@ -1,109 +1,32 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #333; padding: 1rem; position: sticky; top: 0;">
   <div class="container">
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-      <form class="form-inline my-2 my-lg-0 search-form">
-        <div class="search-wrapper" style="position: relative; display: flex; align-items: center;">
-          <!-- Input field with padding on the left to make room for the icon -->
-          <input class="search-input form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-            style="padding-left: 40px; width: 100%; padding-right: 30px;">
-          <!-- Search icon inside the input -->
-          <button class="search-button" type="submit"
-            style="position: absolute; left: 10px; top: 50%; transform: translateY(-50%); background: transparent; border: none; padding: 0;">
-            <img src="/velvetandvine/images/search-icon.png" alt="Search" style="width: 20px; height: 20px;">
-          </button>
-        </div>
-      </form>
-
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/new">New!</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/tops">Tops</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/dresses">Dresses</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/bottoms">Bottoms</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/denims">Denims</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/accessories">Accessories</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/jackets">Jackets</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link" href="/velvetandvine/catalog/sale">Sale</a>
         </li>
       </ul>
-
-      <?php
-      if ($this->isAuthenticated() && $this->isAdmin()) {
-
-      ?>
-
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Admin
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/profile.php">Profile</a></li>
-              <li><a class="dropdown-item" href="/velvetandvine/admin/manageinventory">Manage Inventory</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="/velvetandvine/views/account/logout.php">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
-
-      <?php
-      } else if ($this->isAuthenticated() && !$this->isAdmin()) {
-      ?>
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Settings
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/profile.php">Profile</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="/velvetandvine/views/account/logout.php">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
-      <?php
-      } else {
-      ?>
-
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="btn btn-outline-light me-2" href="/velvetandvine/account/register">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="btn btn-light" href="/velvetandvine/account/login">Login</a>
-          </li>
-        </ul>
-
-      <?php
-      }
-      ?>
     </div>
   </div>
 </nav>
