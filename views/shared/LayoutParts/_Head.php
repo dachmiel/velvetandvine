4a38c37d2fbe77a8432860f63eb8d5dd07604de4
Daniel <danielchm6@gmail.com>
2024-11-28 16:39:00 -0500

    Change title tag on 404 error

--- views/shared/LayoutParts/_Head.php
+++ views/shared/LayoutParts/_Head.php
@@ -1,7 +1,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Velvet&Vine</title>
+    <?php
+    if (http_response_code() == 404) { ?>
+        <title>404 Not Found - Velvet & Vine</title>
+    <?php
+    } else {
+    ?>
+        <title>Velvet&Vine</title>
+    <?php
+    }
+    ?>
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Custom CSS -->
