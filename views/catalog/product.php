516d964811e3109cafaa6b67fcadbe6f60147574
Daniel <danielchm6@gmail.com>
2024-11-28 20:31:15 -0500

    Fix no image link

--- views/catalog/product.php
+++ views/catalog/product.php
@@ -3,7 +3,7 @@
     <div class="container mt-4">
         <div class="row align-items-center">
             <div class="col-md-6">
-                <img src="/velvetandvine/images/no-image.png" alt="<?php echo $model->name; ?>" style="width: 100%; height: 100%;">
+                <img src="/velvetandvine/images/no-image.jpg" alt="<?php echo $model->name; ?>" style="width: 100%; height: 100%;">
             </div>
             <div class="col-md-6">
                 <h1><?php echo $model->name; ?></h1>
