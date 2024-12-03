9e823ef8ce7a43af85f1356465a6d307e2d26bc6
Daniel <danielchm6@gmail.com>
2024-12-03 17:32:45 -0500

    Edit error controller

--- controllers/errorController.php
+++ controllers/errorController.php
@@ -3,13 +3,9 @@ include_once "base/baseController.php";
 
 class ErrorController extends BaseController
 {
-    public function Index($id = null)
-    {
-        $this->view("Views/error/index.php");
-    }
 
     public function Show404()
     {
-        $this->view("views/error/404.php");
+        $this->view('404');
     }
 }
