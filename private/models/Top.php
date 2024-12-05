5852eb1c5a8764a17f23a463c6c533990e6df1bb
Daniel <danielchm6@gmail.com>
2024-12-04 20:54:45 -0500

    Create Top model

---
+++ private/models/Top.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace VelvetAndVine\Models;
+
+class Top
+{
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+
+    public function __construct($id, $name, $description, $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+}
