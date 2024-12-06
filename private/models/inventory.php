6fd7e42258819241867040e000e1915ca518df76
lucydisalvo <91683692+lucydisalvo@users.noreply.github.com>
2024-12-06 11:54:01 -0500

    add documentation to inventory.php

--- private/models/inventory.php
+++ private/models/inventory.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * File Name: inventory.php
+ * Purpose: This file holds the Inventory class which initializes an array to 
+ * hold the products of the website. 
+ * Version: 1.0
+ * 
+ */
 class Inventory
 {
     public $products = [];
