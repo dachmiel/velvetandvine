350c9ab78aad4e11c0b233b93da8abf69f74c1fe
lucydisalvo <91683692+lucydisalvo@users.noreply.github.com>
2024-12-06 11:54:24 -0500

    add documentation to product.php

--- private/models/product.php
+++ private/models/product.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File Name: poduct.php
+ * Purpose: This file contains the Product class that holds the product details 
+ * such as product ID, name, description of product, price, quantity of stock, and category ID.
+ * Version: 1.0
+ * 
+ */
 class Product
 {
     public $productID;
