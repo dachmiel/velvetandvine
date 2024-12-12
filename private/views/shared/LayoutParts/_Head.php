145d55ced22d761c7a6fdb06eb15d2d3dde54e51
jjness18 <91549858+jjness18@users.noreply.github.com>
2024-12-11 21:10:08 -0500

    add Font Awesome

--- private/views/shared/LayoutParts/_Head.php
+++ private/views/shared/LayoutParts/_Head.php
@@ -15,6 +15,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/velvetandvine/public/css/styles.css">
+    <!-- Social Media Logos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <!-- Custom font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
