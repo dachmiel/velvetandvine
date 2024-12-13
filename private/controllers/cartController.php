6654567e2ddb833d402b63ea27e8fb4a8163a9f3
Bennett Ferrari <baferrar@syr.edu>
2024-12-13 18:01:54 -0500

    Add cart models and maybe view cart in cart controller

--- private/controllers/cartController.php
+++ private/controllers/cartController.php
@@ -4,9 +4,54 @@ include_once "base/baseController.php";
 include_once __DIR__ . "/../views/account/cart.php";
 include_once __DIR__ . "/../viewModels/cartViewModels.php";
 include_once __DIR__ . "/../models/db.php";
+include_once __DIR__ . "/../models/cartModel.php";
+include_once __DIR__ . "/../models/ApplicationUser.php";
 
 class CartController extends BaseController
 {
 
-    public function viewCart() {}
+    public function viewCart()
+    {
+
+        if (!$this->isAuthenticated()) {
+            header("Location: /velvetandvine/account/login");
+            exit;
+        }
+
+        $userId = $_SESSION['userid'];
+
+        $cartModel = new CartModel();
+
+        $cart = $cartModel->getCartByUserId($userId);
+
+        if (!$cart) {
+            $cartID = $cartModel->createEmptyCart($userId);
+            $cartViewModel = new CartViewModel($cartID, $userId, date('Y-m-d H:i:s'), 0.00);
+        } else {
+            $cartViewModel = new CartViewModel(
+                $cart['CartID'],
+                $cart['UserID'],
+                $cart['CreatedDate'],
+                $cart['TotalAmount']
+            );
+        }
+
+        $cartItems = $cartModel->getCartItems($cartViewModel->CartID);
+
+        $cartItemViewModels = [];
+        foreach ($cartItems as $item) {
+            $cartItemViewModels[] = new CartItemViewModel(
+                $item['CartItemID'],
+                $item['CartID'],
+                $item['ProductID'],
+                $item['Quantity'],
+                $item['Subtotal']
+            );
+        }
+
+        $this->view('cart', [
+            'cartViewModel' => $cartViewModel,
+            'cartItemViewModels' => $cartItemViewModels
+        ]);
+    }
 }
