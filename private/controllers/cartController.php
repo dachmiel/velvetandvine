2bd0f8349abbd9104a991d9a9b00a3c48dd51aca
Bennett Ferrari <baferrar@syr.edu>
2024-12-13 17:34:29 -0500

    Add basic files

---
+++ private/controllers/cartController.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once "base/baseController.php";
+include_once __DIR__ . "/../views/account/cart.php";
+include_once __DIR__ . "/../viewModels/cartViewModels.php";
+include_once __DIR__ . "/../models/db.php";
+
+class CartController extends BaseController
+{
+
+    public function viewCart() {}
+}
---
+++ private/viewModels/cartViewModels.php
@@ -0,0 +1,5 @@
+<?php
+
+class CartViewModel {}
+
+class CartItemViewModel {}
